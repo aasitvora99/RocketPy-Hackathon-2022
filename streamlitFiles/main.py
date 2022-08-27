1f458cb329cf3643714fa3b293147636a10e0d90
Aasit Vora <aasitvora1999@gmail.com>
2022-08-27 18:46:47 +0530

    Initial Commit: Creating Template for streamlit application

---
+++ streamlitFiles/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+header = st.container()
+dataset = st.container()
+features = st.container()
+model_training = st.container()
+with header:
+    st.title("RocketPy")
+    #st.markdown("[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)[![Open in Colab](https://colab.research.google.com/github/giovaniceotto/rocketpy/blob/master/docs/notebooks/getting_started_colab.ipynb)](https://colab.research.google.com/github/giovaniceotto/rocketpy/blob/master/docs/notebooks/getting_started_colab.ipynb)")
+    st.header("Detailed instructions to be added later")
