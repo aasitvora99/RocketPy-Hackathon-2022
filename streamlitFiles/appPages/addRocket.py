0ff50925be143af76a0492861f5ea00bcd8d596d
Aasit Vora <aasitvora1999@gmail.com>
2022-10-16 20:01:07 +0530

    general commit

--- streamlitFiles/appPages/addRocket.py
+++ streamlitFiles/appPages/addRocket.py
@@ -88,20 +88,11 @@ def app():
                                 "streamlitFiles\__pycache__", powerOnDrag.name
                             )
                         st.success("File Uploaded")
-
-            setRailButtons = st.slider(
-                "Set Rail button position relative to CG",
-                -5.0,
-                5.0,
-                (-0.5, 0.2),
-                step=0.01,
-                help="Two values which represent the distance of each of the two rail buttons to the center of mass of the rocket without propellant. If the rail button is positioned above the center of mass, its distance should be a positive value. If it is below, its distance should be a negative value. The order does not matter. All values should be in meters.",
-            )
-
+            if "rocketInitialized" not in st.session_state:
+                st.session_state.rocketInitialized = False
             motor = st.session_state.motor
-            setRailButtons = list(setRailButtons)
             rocketSubmitted = st.button("Submit")
-            if rocketSubmitted:
+            if rocketSubmitted == True or st.session_state.rocketInitialized == True:
                 rokit = Rocket(
                     motor=motor,
                     radius=radius,
@@ -113,5 +104,109 @@ def app():
                     powerOffDrag=powerOffDrag,
                     powerOnDrag=powerOnDrag,
                 )
-                rokit.setRailButtons(setRailButtons)
+                st.session_state.rocketInitialized = True
+
                 st.success("Rocket Added")
+                with aeroTab:
+                    setRailButtons = st.slider(
+                        "Set Rail button position relative to CG",
+                        -5.0,
+                        5.0,
+                        (-0.5, 0.2),
+                        step=0.01,
+                        help="Two values which represent the distance of each of the two rail buttons to the center of mass of the rocket without propellant. If the rail button is positioned above the center of mass, its distance should be a positive value. If it is below, its distance should be a negative value. The order does not matter. All values should be in meters.",
+                    )
+                    setRailButtons = list(setRailButtons)
+                    rokit.setRailButtons(setRailButtons)
+
+                    col1, col2, col3 = st.columns(3)
+                    with col1:
+                        noseLength = st.number_input(
+                            "Add Nosecone Length",
+                            value=0.55829,
+                            min_value=0.0,
+                            format="%f",
+                            help="Nose cone length or height in meters. Must be a positive value.",
+                        )
+                        noseKind = st.selectbox(
+                            "Nosecone Shape Type",
+                            options=("Von Karman", "conical", "ogive", " lvhaack"),
+                        )
+                        noseDistanceToCM = st.number_input(
+                            "Distance between unloaded rocket CM and base of nose",
+                            min_value=0.0,
+                            value=0.71971,
+                            format="%f",
+                            help="Nose cone position relative to rocket unloaded center of mass, considering positive direction from center of mass to nose cone. Consider the center point belonging to the nose cone base to calculate distance.",
+                        )
+
+                        rokit.addNose(
+                            length=noseLength,
+                            kind=noseKind,
+                            distanceToCM=noseDistanceToCM,
+                        )
+
+                    with col2:
+                        # finShape= st.selectbox("Fin Shape Type:", options=("Trapezoidal", "Elliptical"))
+                        st.write("Adding Trapezoidal finset")
+                        finCount = st.number_input(
+                            "# of Fins", min_value=0, value=4, format="%d"
+                        )
+                        finSpan = st.number_input(
+                            "Fin Span",
+                            min_value=0.0,
+                            value=0.1,
+                            format="%f",
+                            help="Fin Span/Height in meters",
+                        )
+                        finRC = st.number_input(
+                            "Root length",
+                            min_value=0.0,
+                            value=0.12,
+                            format="%f",
+                            help="Fin Root Length in meters",
+                        )
+                        finTC = st.number_input(
+                            "Tip length",
+                            min_value=0.0,
+                            value=0.04,
+                            format="%f",
+                            help="Fin Tip Length in meters",
+                        )
+                        finDistanceToCM = st.number_input(
+                            "Distance between unloaded rocket CM and base of nose",
+                            min_value=0.0,
+                            value=0.71971,
+                            format="%f",
+                            help="Fin set position relative to rocket unloaded center of mass, considering positive direction from center of mass to nose cone. Consider the center point belonging to the top of the fins to calculate distance.",
+                        )
+                        rokit.addFins(
+                            finCount,
+                            finSpan,
+                            finRC,
+                            finTC,
+                            finDistanceToCM,
+                            radius=0,
+                            cantAngle=0,
+                        )
+                        # if finShape == "Trapezoidal":
+                        #     rokit.addFin(
+                        #         finCount,
+                        #         finSpan,
+                        #         finRC,
+                        #         finTC,
+                        #         finDistanceToCM,
+                        #         radius=0,
+                        #         cantAngle=0,
+                        #     )
+                        # else:
+                        #     rokit.addEllipticalFins(finCount, finRC, finSpan, finDistanceToCM, cantAngle=)
+
+                    with col3:
+                        st.number_input
+
+            # else:
+            #     with aeroTab:
+            #         st.warning("Initialize rocket first")
+            #     with parachuteTab:
+            #         st.warning("Initialize rocket first")
